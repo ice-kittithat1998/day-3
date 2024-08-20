26eaeeb04636f720ee53f84f07060fb2beb2a6e1
ice-kittithat1998 <kittithat1998@gmail.com>
2024-08-20 16:51:50 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-screen flex justify-center items-center gap-5">
+    <div class="w-[500px] h-[500px] grid grid-row-1 text-center border-4 rounded-lg border-gray-950 animate-bounce  ">
+      <div class=" bg-rose-600  flex items-center justify-center">box1</div>
+      <div class=" bg-blue-600 flex items-center justify-center">box2</div>
+    </div>
+    <div class="w-[500px] h-[500px] grid grid-cols-2 text-center border-4 rounded-lg border-gray-950 animate-pulse">
+      <div class=" bg-rose-600 flex items-center justify-center">box1</div>
+      <div class=" bg-blue-600 flex items-center justify-center">box2</div>
+    </div>
+    <div class="w-[500px] h-[500px] grid grid-cols-3 text-center border-4 rounded-lg border-gray-950">
+      <div class=" bg-rose-600 flex items-center justify-center">box1</div>
+      <div class=" bg-blue-600 flex items-center justify-center">box2</div>
+      <div class=" bg-rose-600 flex items-center justify-center">box3</div>
+    </div>
+  </div>
+</template>
